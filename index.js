10b5b767779c92b11e2db34783a98bf50e387e99
Michelle <nyamburamichelle400@gmail.com>
2024-04-14 21:00:02 +0300

    does lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Add your code here
+let api="http://localhost:3000/users"
+     //console.log(api)
+     function submitData(name, email) {
+      const data = {
+        name: name,
+        email: email
+      };
+    
+      const jsonData = JSON.stringify(data);
+    
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: jsonData
+      };
+    
+      return fetch('http://localhost:3000/users', requestOptions)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          document.body.innerHTML += `<div>${data.id}</div>`;
+          return data;
+        })
+        .catch(error => {
+          if (error.message.includes('Unauthorized Access')) {
+            document.body.innerHTML += `<div>Error: Unauthorized Access</div>`;
+          } else {
+            document.body.innerHTML += `<div>Error: ${error.message}</div>`;
+          }
+          throw error;
+        });
+    }
+    
+      
+      
